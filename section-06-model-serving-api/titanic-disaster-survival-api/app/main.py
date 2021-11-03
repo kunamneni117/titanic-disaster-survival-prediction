6da0d940d54c7032a096a57a1b8fd964fe513dcf
--replace-all <venkateshchowdary117@gmail.com>
2021-11-03 13:37:25 +0530

    updated logging in the model serving api

---
+++ section-06-model-serving-api/titanic-disaster-survival-api/app/main.py
@@ -0,0 +1,6 @@
+from typing import Any
+
+from fastapi import APIRouter, FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from loguru import logger

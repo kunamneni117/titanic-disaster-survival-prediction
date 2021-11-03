a625891365deb856970196b8d8e6756f53881c37
--replace-all <venkateshchowdary117@gmail.com>
2021-11-03 17:52:17 +0530

    adding extra index url to fetch the package from private repo (GEMFURY)

--- 03-model-serving-api/titanic-disaster-survival-api/app/api.py
+++ 03-model-serving-api/titanic-disaster-survival-api/app/api.py
@@ -6,6 +6,7 @@ import pandas as pd
 from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
 from loguru import logger
+
 from classification_model import __version__ as model_version
 from classification_model.predict import make_prediction
 
